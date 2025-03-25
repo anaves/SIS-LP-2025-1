2aa34ff8148968311a52ee588b7d916dbf73ba36
Alysson Naves <alysson.naves@ectare.com.br>
2025-03-25 08:06:17 -0300

    update: importar biblioteca

---
+++ aulas/003_bibliotecas/exemplo01.py
@@ -0,0 +1,12 @@
+import math
+# import math as m   # forma opcional com alias m
+# import pandas
+print(math.sqrt(16))
+# print(m.sqrt(16))      forma opcional
+potencia = math.pow(2,8)
+print(potencia)
+print(2**8)
+# tabela = pandas.read_csv("https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv")
+# print(tabela.head)
+# print(tabela.info())
+print(abs(-98))
