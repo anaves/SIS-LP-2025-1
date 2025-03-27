34abf91a3c3bb626302e7de8d2f19918d2935955
Alysson Naves <alysson.naves@ectare.com.br>
2025-03-26 21:53:47 -0300

    feat: aula 26mar

---
+++ aulas/004_condicionais/exemplo01_if.py
@@ -0,0 +1,16 @@
+numero = -4
+p = numero > 0   # verificacao Verdadeira - True
+print(f"valor de p: {p}")
+# print(type(p))   # tipo booleana - bool
+outro_numero = 1
+q = outro_numero > 0
+print(f"valor de q: {q}")
+np = not p
+print(f"negacao de p: {np}")
+r = p and q
+print(f"p E q: {r}")
+
+if p:
+    print(f"{numero} eh maior que zero")
+
+print("ok")
